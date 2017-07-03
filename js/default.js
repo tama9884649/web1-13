b6b8770f820a61e1707673e9b0b05527fed37ff1
tama9884649 <sashoyaruki0@docomo.ne.jp>
2017-07-03 17:46:11 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,3 +1,21 @@
+function getFileName() {
+
+return window.location.href.split('/').pop();
+}
+
+var filename = getFileName();
+var opt;
+if(filename === 'other.html'){
+opt = document.querySelector('option[value="other.html"]');
+}else{
+opt = document.querySelector('option[index="other.html"]');
+}
+opt.selected = true;
+
+
+document.getElementById('form').select.onchange = function(){
+location.href = document.getElementById('form').select.value;
+}
 var separate_time = function(time){
 var sec = Math.floor((time / 1000) % 60);
 var min = Math.floor((time / 1000 / 60) % 60);
